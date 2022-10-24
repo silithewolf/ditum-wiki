5092ec19a26f7e30147df5e90f8f23c765a00b17
Silas Wolf <68241785+silithewolf@users.noreply.github.com>
2022-10-24 12:15:33 +0200

    customizing again

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,43 +1,45 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Lernen und Lehren",
+    Svg: require("@site/static/img/educator.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Lehrkräfte müssen sich stetig weiterbilden. Diese Website hilft dir
+        dabei, den Lehr- und Lernprozess digitaler zu gestalten.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Kinderleicht",
+    Svg: require("@site/static/img/easy.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Digitalisierung ist nicht einfach - aber dieses Wiki macht die
+        Einbindung digitaler Tools und Methoden in deinen Unterricht super
+        einfach!
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Community",
+    Svg: require("@site/static/img/community.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Dieses Wiki ist Open Source. Jeder kann demnach Änderungs- und
+        Ergänzungsanträge stellen, die bei der Weiterentwicklung dieser Sammlung
+        mit einbezogen werden.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
